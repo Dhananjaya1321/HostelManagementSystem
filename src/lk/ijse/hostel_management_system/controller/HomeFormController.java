773619d797948b8600b5c30da22843d27d4177d9
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-03-27 18:44:50 -0800

    Created HomeForm and HomeFormController class

---
+++ src/lk/ijse/hostel_management_system/controller/HomeFormController.java
@@ -0,0 +1,48 @@
+package lk.ijse.hostel_management_system.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+
+public class HomeFormController {
+
+    public BorderPane pane;
+    @FXML
+    private GridPane rightPane;
+
+    @FXML
+    private AnchorPane clockPane;
+
+    @FXML
+    void btnDashboard(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegistration(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRooms(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSettings(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnStudent(ActionEvent event) {
+
+    }
+
+}
--- src/lk/ijse/hostel_management_system/controller/LogInFormController.java
+++ src/lk/ijse/hostel_management_system/controller/LogInFormController.java
@@ -1,5 +1,26 @@
 package lk.ijse.hostel_management_system.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
 public class LogInFormController {
 
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    private JFXPasswordField pswdPassword;
+
+    @FXML
+    private JFXTextField txtPassword;
+
+    public void btnLogInOnAction(ActionEvent actionEvent) {
+
+    }
 }
