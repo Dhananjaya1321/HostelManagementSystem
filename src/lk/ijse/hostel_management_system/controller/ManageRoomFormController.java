1acf207924cce5a5da9362cad6754bf38dcffc1b
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-03-28 23:22:04 -0800

    Created ManageRoomForm and ManageRoomFormController class

---
+++ src/lk/ijse/hostel_management_system/controller/ManageRoomFormController.java
@@ -0,0 +1,42 @@
+package lk.ijse.hostel_management_system.controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class ManageRoomFormController {
+
+    @FXML
+    private GridPane rightPane;
+
+    @FXML
+    private Label lblType;
+
+    @FXML
+    private TextField txtRoomQTY;
+
+    @FXML
+    private JFXComboBox<?> cmbRoomTypeID;
+
+    @FXML
+    private TextField txtKeyMoney;
+
+    @FXML
+    void btnAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdate(ActionEvent event) {
+
+    }
+
+}
