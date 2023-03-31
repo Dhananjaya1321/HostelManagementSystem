d9014501d07f8475e7a3f1323b113b5537ea62a7
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-03-31 15:26:20 -0800

    Created FactoryConfiguration class

---
+++ src/lk/ijse/hostel_management_system/util/FactoryConfiguration.java
@@ -0,0 +1,32 @@
+package lk.ijse.hostel_management_system.util;
+
+import lk.ijse.hostel_management_system.entity.Reservation;
+import lk.ijse.hostel_management_system.entity.Room;
+import lk.ijse.hostel_management_system.entity.Student;
+import lk.ijse.hostel_management_system.entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+    private FactoryConfiguration() {
+        sessionFactory=new Configuration()
+                .mergeProperties()
+                .addAnnotatedClass(User.class)
+                .addAnnotatedClass(Room.class)
+                .addAnnotatedClass(Reservation.class)
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance() {
+        return factoryConfiguration == null ? factoryConfiguration = new FactoryConfiguration() : factoryConfiguration;
+    }
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+
+}
